559216ee2617535e47911cb72a18f02f7a8e6569
Gowtham-Murugan <167169250+Gowtham-Murugan@users.noreply.github.com>
2025-12-20 18:40:00 +0530

    DriverFactory completed

---
+++ src/test/java/com/vishall/framework/driver/DriverFactory.java
@@ -0,0 +1,69 @@
+package com.vishall.framework.driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class DriverFactory {
+
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    //creates new browser instance
+    public static void initialize_driver(String browser){
+
+        if (browser==null || browser.isBlank()){
+            throw new RuntimeException("Browser value is missing");
+        }
+
+        if(driver.get()==null){
+            switch(browser.toLowerCase()){
+                case "chrome":
+                    driver.set(createChromeDriver());
+                    break;
+                case "edge":
+                    driver.set(createEdgeDriver());
+                    break;
+                case "firefox":
+                    driver.set(createFireFoxDriver());
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unsupported browser name: "+browser);
+            }
+            driver.get().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        }
+    }
+
+    //returns the WebDriver instance for the current thread
+    public static WebDriver getDriver(){
+        if(driver.get()==null){
+            throw new IllegalStateException("Driver not initialized");
+        }
+        return driver.get();
+    }
+
+    //closes the browser
+    public static void quitDriver(){
+        if (driver.get()!=null){
+            driver.get().quit();
+            driver.remove();
+        }
+    }
+
+
+    private static WebDriver createChromeDriver(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        return new ChromeDriver(options);
+    }
+    private static WebDriver createEdgeDriver(){
+        return new EdgeDriver();
+    }
+    private static WebDriver createFireFoxDriver(){
+        return new FirefoxDriver();
+    }
+
+}
