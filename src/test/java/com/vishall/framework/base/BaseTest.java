0c0265ac6721bcd35e5dc10ef9c75cd134412f81
Gowtham-Murugan <167169250+Gowtham-Murugan@users.noreply.github.com>
2025-12-21 12:03:35 +0530

    Config file and reader setup; BaseTest setup completed

---
+++ src/test/java/com/vishall/framework/base/BaseTest.java
@@ -0,0 +1,23 @@
+package com.vishall.framework.base;
+
+import com.vishall.framework.driver.DriverFactory;
+import com.vishall.framework.utils.ConfigReader;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    @BeforeMethod
+    //create browser for current test thread
+    public void setup(){
+        //initializes the requested browser
+        DriverFactory.initialize_driver(ConfigReader.getProp("browser"));
+        //opens url
+        DriverFactory.getDriver().get(ConfigReader.getProp("url"));
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        DriverFactory.quitDriver();
+
+    }
+}
