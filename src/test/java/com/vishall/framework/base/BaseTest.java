b50bcda0bacf58786be42dea664e573f9947462c
vishall <vishalljk290@gmail.com>
2026-01-26 21:34:08 +0530

    Removed listeners from testng.xml and added them in BaseTest class @Listeners(TestListener.class)

--- src/test/java/com/vishall/framework/base/BaseTest.java
+++ src/test/java/com/vishall/framework/base/BaseTest.java
@@ -1,10 +1,13 @@
 package com.vishall.framework.base;
 
 import com.vishall.framework.driver.DriverFactory;
+import com.vishall.framework.listeners.TestListener;
 import com.vishall.framework.utils.ConfigReader;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners(TestListener.class)
 public class BaseTest {
     @BeforeMethod
     //create browser for current test thread
