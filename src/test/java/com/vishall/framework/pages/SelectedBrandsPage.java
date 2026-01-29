a6f06b55d7b3f7b46382bfe593964575008bb144
vishall <vishalljk290@gmail.com>
2026-01-29 07:44:17 +0530

    DataProvider appropriate code modification part 2

--- src/test/java/com/vishall/framework/pages/SelectedBrandsPage.java
+++ src/test/java/com/vishall/framework/pages/SelectedBrandsPage.java
@@ -6,11 +6,18 @@ import org.openqa.selenium.By;
 public class SelectedBrandsPage extends BasePage {
     By sortByMenu = By.xpath("//select[@name='s']");
 
+    private By findElementUsingProductName(String productName){
+         return By.xpath("//a[.//h2[@aria-label='"+productName+"']]");
+    }
+
     public void sortThePage(){
         selectByVisibleText(sortByMenu, "Price: Low to High");
     }
 
+    //This method will call findElementUsingProductName method and will send the value received from DataProvider to it
+    //This value sent will be used as a custom value and xpath will be built on it
     public void clickOnProductName(){
-        clickUsingProductName("Iconic Swan Bangle, Swan, Black, Rose Gold-Tone Plated");
+        By productNameElement = findElementUsingProductName("Iconic Swan Bangle, Swan, Black, Rose Gold-Tone Plated");
+        click(productNameElement);
     }
 }
--- src/test/java/com/vishall/framework/pages/WomensFashionPage.java
+++ src/test/java/com/vishall/framework/pages/WomensFashionPage.java
@@ -1,10 +1,16 @@
 package com.vishall.framework.pages;
 
 import com.vishall.framework.base.BasePage;
+import org.openqa.selenium.By;
 
 public class WomensFashionPage extends BasePage{
 
+    private By brandElement(String brandName) {
+        return By.xpath("//a[.//img[contains(@src,'" + brandName + "')]]");
+    }
+
     public void selectBrand(String brandName){
-        clickUsingBrandName(brandName);
+        By brandElementName = brandElement(brandName);
+        click(brandElementName);
     }
 }
