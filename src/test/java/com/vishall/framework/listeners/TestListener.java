118e34aff00cfd88ec51c1e536000cbbea1c1e49
vishall <vishalljk290@gmail.com>
2026-01-30 14:08:37 +0530

    Integrated extent report in Listeners

--- src/test/java/com/vishall/framework/listeners/TestListener.java
+++ src/test/java/com/vishall/framework/listeners/TestListener.java
@@ -1,6 +1,10 @@
 package com.vishall.framework.listeners;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import com.vishall.framework.driver.DriverFactory;
+import com.vishall.framework.reports.ExtentManager;
+import com.vishall.framework.reports.ExtentTestManager;
 import com.vishall.framework.utils.LoggersUtil;
 import com.vishall.framework.utils.ScreenShotUtil;
 import org.apache.commons.io.FileUtils;
@@ -17,7 +21,10 @@ import org.apache.logging.log4j.Logger;
 
 public class TestListener implements ITestListener {
 
+    //Logger instance
     private static final Logger log = LoggersUtil.getLogger(TestListener.class);
+    //ExtentReports instance
+    private static ExtentReports extent = ExtentManager.getInstance();
 
 
     @Override
@@ -28,6 +35,10 @@ public class TestListener implements ITestListener {
     @Override
     public void onTestStart(ITestResult result){
         String testName = result.getMethod().getMethodName();
+
+        ExtentTest extentTest = extent.createTest(testName);
+        ExtentTestManager.setTest(extentTest);
+
         log.info("Test started: {}", testName);
     }
 
@@ -36,6 +47,9 @@ public class TestListener implements ITestListener {
         String testName = result.getMethod().getMethodName();
 
         ScreenShotUtil.takeScreenshot(result);
+
+        ExtentTestManager.getTest().pass("Test passed");
+
         log.info("Test passed: {}", testName);
     }
 
@@ -44,6 +58,9 @@ public class TestListener implements ITestListener {
         String testName = result.getMethod().getMethodName();
 
         ScreenShotUtil.takeScreenshot(result);
+
+        ExtentTestManager.getTest().fail(result.getThrowable());
+
         log.error("Test failed: {}", testName);
 
         if (result.getThrowable() != null){
@@ -54,11 +71,15 @@ public class TestListener implements ITestListener {
     @Override
     public void onTestSkipped(ITestResult result){
         String testName = result.getMethod().getMethodName();
+
+        ExtentTestManager.getTest().skip("Test skipped");
+
         log.warn("Test failed: {}", testName);
     }
 
     @Override
     public void onFinish(ITestContext context){
+        extent.flush();
         log.info("Suite Finished: {}", context.getName());
     }
 }
