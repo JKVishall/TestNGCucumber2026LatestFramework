446af2cb91f0756a165447f052ceee1066b20d9a
vishall <vishalljk290@gmail.com>
2026-01-31 18:45:14 +0530

    Extent reports integrated screenshots

--- src/test/java/com/vishall/framework/utils/ScreenShotUtil.java
+++ src/test/java/com/vishall/framework/utils/ScreenShotUtil.java
@@ -8,6 +8,9 @@ import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -18,10 +21,35 @@ public class ScreenShotUtil {
 
         if (driver != null) {
             try {
+                //Takes screenshot and stores in temp File
                 File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-                screenshotPath = "screenshots/" + result.getMethod().getMethodName() +
-                        LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss_SSS")) + ".png";
-                File dest = new File(screenshotPath);
+
+
+
+                // 2) Ensure output directory exists
+                Path folder = Paths.get("test-output", "screenshots");
+                Files.createDirectories(folder);
+
+
+                //Custom date and time formats
+                LocalDateTime now = LocalDateTime.now();
+                DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH-mm-ss");
+
+                //Custom name format
+                String testCaseName = "Test case_" + result.getMethod().getMethodName();
+
+                //Setting the custom data time format to data and time variables
+                String date = now.format(dateFormatter);
+                String time = now.format(timeFormatter);
+
+
+                //setting them all up together
+                screenshotPath = testCaseName + "_Date_" + date + "_Time_" + time + ".png";
+
+               /* screenshotPath = "test-output/screenshots/"+result.getMethod().getMethodName() +
+                        LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss_SSS")) + ".png";*/
+                File dest = new File("test-output/screenshots/"+screenshotPath);
 
                 FileUtils.copyFile(src, dest);
 
@@ -29,6 +57,6 @@ public class ScreenShotUtil {
                 e.printStackTrace();
             }
         }
-        return screenshotPath;
+        return "screenshots/"+screenshotPath;
     }
 }
