85fa5873d02946ce056a149718ecdcd9453c3536
vishall <vishalljk290@gmail.com>
2026-02-02 18:27:55 +0530

    Comments added for multiple files. Groups created

--- src/test/java/com/vishall/framework/tests/SmokeTest.java
+++ src/test/java/com/vishall/framework/tests/SmokeTest.java
@@ -5,7 +5,7 @@ import com.vishall.framework.driver.DriverFactory;
 import org.testng.annotations.Test;
 
 public class SmokeTest extends BaseTest {
-    @Test
+    @Test(groups={"smoke"})
     public void verifyAppLaunchandURLOpen() {
         String url = DriverFactory.getDriver().getCurrentUrl();
         System.out.println(url);
