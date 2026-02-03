0180cef651872de0b7325c23373c00b57d5fef60
vishall <vishalljk290@gmail.com>
2026-02-03 23:02:02 +0530

    revert groups

--- src/test/java/com/vishall/framework/tests/WomensFashionNavTest.java
+++ src/test/java/com/vishall/framework/tests/WomensFashionNavTest.java
@@ -32,7 +32,7 @@ public class WomensFashionNavTest extends BaseTest {
     }
 
 
-    @Test(groups = "regression", dataProvider="testDataForWomensFashionNavTest")
+    @Test(dataProvider="testDataForWomensFashionNavTest")
     public void addMaxCountOfProductToCart(HashMap<String, String> data){
         womensFashion = new WomensFashionNav();
 
